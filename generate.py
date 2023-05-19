8ab675922ca2b0e0427b18bec24ea5f3d43519c0
liutiedong <e0325301@u.nus.edu>
2023-05-19 11:25:22 +0800

    update

--- generate.py
+++ generate.py
@@ -72,11 +72,6 @@ def main(
             device_map={"": device},
         )
 
-    # unwind broken decapoda-research config
-    # model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
-    # model.config.bos_token_id = 1
-    # model.config.eos_token_id = 2
-
     if not load_8bit:
         model.half()  # seems to fix bugs for some users.
 
